c598fd159eecdd97d49eb759a858db840413d91a
Stefano Cicatiello <stefano.cicatiello@gmail.com>
2017-05-18 11:41:31 +0200

    fixes and improvements

--- src/index.js
+++ src/index.js
@@ -12,10 +12,10 @@ export default class Swapper {
 
         this._findElementsInThePage();
 
+        if (this.isEnabled) {
+            this.enable();
+        }
         [].forEach.call(this.boxes, (box) => {
-            if (this.isEnabled) {
-                box.setAttribute('draggable', this.isEnabled);
-            }
             box.addEventListener('dragstart', e => this.handleDragStart(e), false);
             box.addEventListener('dragenter', e => this.handleDragEnter(e), false);
             box.addEventListener('dragover', e => this.handleDragOver(e), false);
@@ -25,17 +25,20 @@ export default class Swapper {
         });
     }
 
+
     get name() {
         return this._name;
     }
 
+
     handleDragStart(e) {
-        e.target.classList.add('swapper-moving');
+        e.target.style.opacity = '0.5';
         this.dragSrcEl = e.target;
 
         e.dataTransfer.effectAllowed = 'move';
         e.dataTransfer.setData('text/html', e.target.innerHTML);
     }
+
 
     handleDragOver(e) {
         if (e.preventDefault) {
@@ -47,26 +50,24 @@ export default class Swapper {
         return false;
     }
 
+
     handleDragEnter(e) {
         if (!this.dragSrcEl || !e.target || this.dragSrcEl === e.target) {
             return;
         }
 
-        if (this.dragSrcEl && e.target) {
-            if (!this._hasSameContainers(this.dragSrcEl, e.target)) {
-                e.target.classList.remove('swapper--over');
-                return;
-            }
-        }
-
         // current hover target
-        e.target.classList.add('swapper--over');
+        if (this._canDrop(this.dragSrcEl, e.target)) {
+            e.target.classList.add('swapper--over');
+        }
     }
+
 
     handleDragLeave(e) {
         // previous target element
         e.target.classList.remove('swapper--over');
     }
+
 
     handleDrop(e) {
         if (e.stopPropagation) {
@@ -74,7 +75,7 @@ export default class Swapper {
         }
 
         // Don't do anything if dropping the same elements we're dragging.
-        if (this._areDifferentElements(this.dragSrcEl, e.target) && this._hasSameContainers(this.dragSrcEl, e.target)) {
+        if (this._canDrop(this.dragSrcEl, e.target)) {
             // Set the source column's HTML to the HTML of the column we dropped on.
             this.dragSrcEl.innerHTML = e.target.innerHTML;
             e.target.innerHTML = e.dataTransfer.getData('text/html');
@@ -104,13 +105,18 @@ export default class Swapper {
         return false;
     }
 
+
     handleDragEnd(e) {
         [].forEach.call(this.boxes, function (col) {
             col.classList.remove('swapper--over');
-            col.classList.remove('swapper--moving');
+            e.target.style.opacity = '1.0';
         });
     }
 
+
+    //
+    // Find all the elements inside all containers
+    //
     _findElementsInThePage() {
         let selector = '';
 
@@ -122,20 +128,34 @@ export default class Swapper {
         this.boxes = document.querySelectorAll(selector);
     }
 
+
+    //
+    // Enable drag
+    //
     enable() {
         this.isEnabled = true;
         [].forEach.call(this.boxes, (box) => {
             box.setAttribute('draggable', true);
+            box.style.cursor = 'move';
         });
     }
 
+
+    //
+    // Disable drag
+    //
     disable() {
         this.isEnabled = false;
         [].forEach.call(this.boxes, (box) => {
             box.removeAttribute('draggable');
+            box.style.cursor = 'default';
         });
     }
 
+
+    //
+    // Find the container of the element
+    //
     _findContainer(element) {
         let container = null;
 
@@ -147,15 +167,39 @@ export default class Swapper {
         return container;
     }
 
+
+    //
+    // Check if the two elements are not the same
+    //
     _areDifferentElements(el1, el2) {
         return el1 && el2 && el1 !== el2;
     }
 
+
+    //
+    // Check if the two elements are in the same container
+    //
     _hasSameContainers(el1, el2) {
         let c1 = this._findContainer(el1);
-
         let c2 = this._findContainer(el2);
 
         return (c1 && c2 && c1.innerHTML === c2.innerHTML);
+    }
+
+
+    //
+    // Check if can swap elements
+    //
+    _canDrop(el1, el2) {
+        if (!el1 || !el2) {
+            return false;
+        }
+        if (!this._areDifferentElements(el1, el2)) {
+            return false;
+        }
+        if (!this.config.swapBetweenContainers && !this._hasSameContainers(el1, el2)) {
+            return false;
+        }
+        return true;
     }
 }
